66c803b6ec573f9dfb3e2b2be6b332285a929cfb
scourtis <scourtis@myune.edu.au>
2024-07-26 21:06:31 +1000

    set up postman for Api's, created get user and post login via bearer token in postman

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+   // return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::get('/user', [UserController::class,'index'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class,'login']);
